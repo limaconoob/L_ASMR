22430188c473ba4e8106713d8508ec64f80ff2ff
Jérémy Pepin <jpepin@student.42.fr>
2018-06-09 03:16:26 +0200

    LED heartbeat sur PicKit3

---
+++ L_ASMR.X/main.c
@@ -0,0 +1,38 @@
+/*
+**    main.c
+** 
+**    L_ASMR is an embedded systems project. The initial purpose is to build a
+**    binaural microphone and some useful features on it, with cheap materials.
+**
+**    Copyright (C) 2018  {name of author}
+**
+**    This program is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    This program is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**
+**    L_ASMR  Copyright (C) 2018  Jérémy Pepin
+**    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+**    This is free software, and you are welcome to redistribute it
+**    under certain conditions; type `show c' for details.
+*/
+
+
+#include <p32xxxx.h>
+#include <sys/attribs.h>
+
+int main()
+{ TRISFbits.TRISF1 = 0;
+  TRISDbits.TRISD8 = 1;
+  LATFbits.LATF1 = 1;
+  while (42)
+  { if (PORTDbits.RD8 == 0)
+    { LATFbits.LATF1 = 0; }}}
